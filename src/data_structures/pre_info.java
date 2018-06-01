33433f75fc99021267d7d86b34de5fc89fb2e4b3
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-01 16:20:27 +0100

    pre_info class defined

---
+++ src/data_structures/pre_info.java
@@ -0,0 +1,19 @@
+package data_structures;
+
+public class pre_info {
+	Astigm pre;
+	Astigm TIA;
+	String surgery;
+	
+	public Astigm getPre() {
+		return pre;
+	}
+	
+	public Astigm getTIA() {
+		return TIA;
+	}
+	
+	public String getSurgery() {
+		return surgery;
+	}
+}
