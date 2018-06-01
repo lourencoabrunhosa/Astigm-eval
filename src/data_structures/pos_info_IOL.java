2841231fb102474ea1302a2c57e9070daf9761ae
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-01 18:10:46 +0100

    pos_info_IOL defined
    creates pos_info for specific surgery type IOL implant

---
+++ src/data_structures/pos_info_IOL.java
@@ -0,0 +1,26 @@
+package data_structures;
+
+public class pos_info_IOL extends pos_info {
+	
+	Astigm pos_cornea;
+	Astigm SIA_cornea;
+	Astigm diff_cornea;
+	Astigm IOL;
+	double CI_cornea;
+	double IOL_axis;
+	
+	public pos_info_IOL(double k1, double k2, double a1, double a2,double S,double C, double A, double IOL_S, double IOL_C, double IOL_A,pre_info preinfo) {
+		super();
+		if(a1>=a2) pos_cornea=new Astigm((k1+k2)/2,k1,a1);
+		else pos_cornea=new Astigm((k1+k2)/2,k2,a2);
+		SIA_cornea=pos_cornea.add(((pre_info_IOL)preinfo).pre_cornea.minus());
+		diff_cornea=((pre_info_IOL)preinfo).TIA_cornea.add(SIA_cornea.minus());
+		CI_cornea=SIA_cornea.proj(((pre_info_IOL)preinfo).TIA_cornea);
+		pos=new Astigm(S,C,A);
+		SIA=pos.add(preinfo.pre.minus());
+		diff=preinfo.TIA.add(SIA.minus());
+		CI=SIA.proj(preinfo.TIA);
+		IOL=new Astigm(IOL_S,IOL_C,IOL_A);
+		IOL_axis=pos.add(pos_cornea.minus()).axis();
+	}
+}
