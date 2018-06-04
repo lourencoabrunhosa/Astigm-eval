70782326be5fb6d69c6ddf08095f40fe78103d24
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-04 12:37:08 +0100

    Created REF_case panel
    Combines Input_REF and Output_REF on the same panel (for easy placement and management of future tabbed pane)

---
+++ src/GUI/REF_case.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class REF_case extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	
+	Base_frame parent;
+	
+	Input_REF input;
+	Output_REF output;
+	
+	public REF_case(Base_frame parent) {
+		this.parent=parent;
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
+		input=new Input_REF(parent);
+		output=new Output_REF(parent);
+		add(input,BorderLayout.LINE_START);
+		add(output,BorderLayout.LINE_END);
+	}
+
+}
