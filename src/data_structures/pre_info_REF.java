9c906c6984be09150f6b4c156f56e9541b8606ce
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-06 17:57:55 +0100

    Corrected formulas for ophtalmic data components

--- src/data_structures/pre_info_REF.java
+++ src/data_structures/pre_info_REF.java
@@ -4,8 +4,8 @@ public class pre_info_REF extends pre_info {
 
 	public pre_info_REF (double k1, double k2, double a1, double a2){
 		super();
-		if(a1>=a2) pre=new Astigm((k1+k2)/2,k1,a1);
-		else pre=new Astigm((k1+k2)/2,k2,a2);
+		if(k1>=k2) pre=new Astigm((k1+k2)/2,k1-k2,a1);
+		else pre=new Astigm((k1+k2)/2,k2-k1,a2);
 		TIA=pre.minus();
 		surgery="Refractive";
 	}
