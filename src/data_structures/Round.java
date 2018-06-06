35fc823623a5e43da24110beb74db576bb68d271
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-06 17:58:25 +0100

    Created Round
    rounds numbers to given number of decimal places

---
+++ src/data_structures/Round.java
@@ -0,0 +1,16 @@
+package data_structures;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Round {
+
+
+public static double round(double value, int places) {
+    if (places < 0) throw new IllegalArgumentException();
+
+    BigDecimal bd = new BigDecimal(value);
+    bd = bd.setScale(places, RoundingMode.HALF_UP);
+    return bd.doubleValue();
+}
+}
