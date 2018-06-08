cf9c7ed51dec4dfb01d1c3ad34fefa95f63bab68
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-08 20:47:31 +0100

    cornea and subjective output frames added

--- src/GUI/Output_IOL.java
+++ src/GUI/Output_IOL.java
@@ -15,8 +15,18 @@ public class Output_IOL extends JTabbedPane {
 	
 	IOL_case parent;
 	
+	Output_IOL_cornea cornea;
+	Output_IOL_subjective subjective;
+	
 	public Output_IOL(IOL_case parent) {
+		
 		this.parent=parent;
+		
+		cornea = new Output_IOL_cornea(this);
+		subjective = new Output_IOL_subjective(this);
+		
+		add("Subjective", subjective);
+		add("Cornea", cornea);
 
 	}
 
