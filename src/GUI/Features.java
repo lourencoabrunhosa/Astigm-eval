8675b11080e93db7bc43e7c17c5d32d1e86edc60
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-08 13:08:55 +0100

    IOL_case added to Features

--- src/GUI/Features.java
+++ src/GUI/Features.java
@@ -17,14 +17,17 @@ public class Features extends JTabbedPane {
 	Base_frame parent;
 	
 	REF_case REF_patient;
+	IOL_case IOL_patient;
 	public Features(Base_frame parent) {
 		this.parent=parent;
 		
 		setPreferredSize(new Dimension(600,400));
 		
 		REF_patient=new REF_case(this);
+		IOL_patient=new IOL_case(this);
 		
 		add("Refractive Surgery",REF_patient);
+		add("IOL implant Surgery",IOL_patient);
 		
 
 	}
