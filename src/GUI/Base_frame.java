32e72870d6cdc6d5cf6d6f7cd86fe6a0f636ff24
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-06-19 00:15:24 +0100

    patient panel created and added to base frame
    still needs design checking and doctor login frame, to have doctor object and create base frame with that parameter

--- src/GUI/Base_frame.java
+++ src/GUI/Base_frame.java
@@ -1,11 +1,12 @@
 package GUI;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import data_structures.doctor;
 
 
 public class Base_frame extends JFrame {
@@ -15,37 +16,45 @@ public class Base_frame extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	
+	public Features features;
+	public Patient_panel patient_panel;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Base_frame frame = new Base_frame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Base_frame frame = new Base_frame(doc);
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
 	 */
-	public Base_frame() {
+	public Base_frame(doctor doc) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 500);
-		setSize(700,500);
+		setBounds(100, 100, 700, 600);
+		setSize(700,600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		setResizable(false);
 		
-		contentPane.add(new Features(this));
+		
+		features=new Features(this);
+		patient_panel=new Patient_panel(this,doc);
+		
+		contentPane.add(features);
+		contentPane.add(patient_panel,BorderLayout.PAGE_START);
 	}
 
 }
---
+++ src/GUI/Patient_panel.java
@@ -0,0 +1,107 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.time.LocalDate;
+import java.util.Locale;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import data_structures.doctor;
+import data_structures.patient;
+
+public class Patient_panel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Create the panel.
+	 */
+	
+	doctor doc;
+	patient patient;
+	
+	Base_frame parent;
+	
+	public Patient_panel(Base_frame parent,doctor doc) {
+		
+		Locale.setDefault(Locale.US);
+		setBackground(Color.WHITE);
+		setPreferredSize(new Dimension(700,100));
+		setLayout(null);
+		
+		this.parent=parent;
+		this.doc=doc;
+		
+		Font Title = new Font("Tahoma",Font.BOLD,16);
+		Font label = new Font("Tahoma",Font.PLAIN,12);
+		
+		
+		JLabel JLabel_1=new JLabel("Patient");
+		JLabel_1.setFont(Title);
+		JLabel_1.setBounds(1,1,150,20);
+		
+		JLabel JLabel_2=new JLabel("Name");
+		JLabel_2.setFont(label);
+		JLabel_2.setBounds(10,5,150,20);
+		
+		JLabel JLabel_3=new JLabel("Surname");
+		JLabel_3.setFont(label);
+		JLabel_3.setBounds(100,10,150,20);
+		
+		JLabel JLabel_4=new JLabel("Birth Date");
+		JLabel_4.setFont(label);
+		JLabel_4.setBounds(5,80,150,20);
+		
+		JLabel JLabel_5=new JLabel("Day");
+		JLabel_5.setFont(label);
+		JLabel_5.setBounds(50,55,150,20);
+		
+		JLabel JLabel_6=new JLabel("Month");
+		JLabel_6.setFont(label);
+		JLabel_6.setBounds(100,55,150,20);
+		
+		JLabel JLabel_7=new JLabel("Year");
+		JLabel_7.setFont(label);
+		JLabel_7.setBounds(150,55,150,20);
+		
+		JTextField name=new JTextField();
+		name.setFont(label);
+		name.setBounds(10,25,100,20);
+		
+		JTextField surname=new JTextField();
+		surname.setFont(label);
+		surname.setBounds(100,25,100,20);
+		
+		JComboBox<Integer> day=new JComboBox<Integer>();
+		for(int i=1;i<=31;i++) {
+			day.addItem(i);
+		}
+		day.setFont(label);
+		day.setBounds(50,80,50,20);
+		
+		JComboBox<Integer> month=new JComboBox<Integer>();
+		for(int i=1;i<=12;i++) {
+			month.addItem(i);
+		}
+		month.setFont(label);
+		month.setBounds(100,80,50,20);
+		
+		JComboBox<Integer> year=new JComboBox<Integer>();
+		int y=LocalDate.now().getYear();
+		for(int i=0;i<=180;i++) {
+			year.addItem(y-i);
+		}
+		year.setFont(label);
+		year.setBounds(150,80,50,20);
+		
+		
+	}
+
+}
