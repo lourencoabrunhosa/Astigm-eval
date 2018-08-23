147ef0e88f2a509dcb5cfc9445b3c020dac5d6de
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-08-23 15:48:30 +0100

    Barcharts receive data matrixes as arguments
    Axis labels and titles also defined

--- src/Graphics/barchart.java
+++ src/Graphics/barchart.java
@@ -5,29 +5,30 @@ import javafx.scene.chart.*;
 
 public class barchart{
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static BarChart<String,Number> barChart() {
+	public static BarChart<String,Number> barChartV(double[][] data) {
 		final CategoryAxis xAxis = new CategoryAxis();
 		final NumberAxis yAxis = new NumberAxis();
 		final BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
 		
-		bc.setTitle("Test");
+		bc.setTitle("Pre-surgery Vs. Post-surgery Astigmatism");
 		
-		xAxis.setLabel("x");
-		yAxis.setLabel("y");
+		xAxis.setLabel("Cylindric power [D]");
+		yAxis.setLabel("Ocurrences [%]");
 		
 		XYChart.Series series1 = new XYChart.Series<>();
 		series1.setName("pre");
 		
-		series1.getData().add(new XYChart.Data<String,Number>("0-0.5",32));
-		series1.getData().add(new XYChart.Data<String,Number>("0.5-1",64));
-		series1.getData().add(new XYChart.Data<String,Number>("1-1.5",96));
-		
 		XYChart.Series series2 = new XYChart.Series<>();
 		series2.setName("pos");
 		
-		series2.getData().add(new XYChart.Data<String,Number>("0-0.5",12));
-		series2.getData().add(new XYChart.Data<String,Number>("0.5-1",24));
-		series2.getData().add(new XYChart.Data<String,Number>("1-1.5",36));
+		double x=0;
+		for(int i=0;i<data[0].length-1;i++) {
+			series1.getData().add(new XYChart.Data<String,Number>(x+" - "+data[0][i],data[1][i]));
+			series2.getData().add(new XYChart.Data<String,Number>(x+" - "+data[0][i],data[2][i]));
+			x=data[0][i];
+		}
+		series1.getData().add(new XYChart.Data<String,Number>("> "+data[0][data[0].length-2],data[1][data[0].length-1]));
+		series2.getData().add(new XYChart.Data<String,Number>("> "+data[0][data[0].length-2],data[2][data[0].length-1]));
 		
 		bc.getData().addAll(series1,series2);
 		
@@ -35,24 +36,26 @@ public class barchart{
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static BarChart<Number,String> barChartH() {
+	public static BarChart<Number,String> barChartH(double[][] data) {
 		final CategoryAxis yAxis = new CategoryAxis();
 		final NumberAxis xAxis = new NumberAxis();
 		final BarChart<Number,String> bc = new BarChart<Number,String>(xAxis,yAxis);
 		
-		bc.setTitle("Test");
+		bc.setTitle("Angle of Error");
 		
-		xAxis.setLabel("x");
-		yAxis.setLabel("y");
+		xAxis.setLabel("Ocurrences [%]");
+		yAxis.setLabel("Angle interval [°]");
 		
 		XYChart.Series series1 = new XYChart.Series<>();
 		series1.setName("pre");
 		
-		series1.getData().add(new XYChart.Data<Number,String>(32,"0-0.5"));
-		series1.getData().add(new XYChart.Data<Number,String>(64,"0.5-1"));
-		series1.getData().add(new XYChart.Data<Number,String>(96,"1-1.5"));
-	
-		
+		series1.getData().add(new XYChart.Data<Number,String>(data[1][0],"< "+data[0][0]));
+		double x=-75;
+		for(int i=1;i<data[0].length-1;i++) {
+			series1.getData().add(new XYChart.Data<Number,String>(data[1][i], x+" - "+data[0][i]));
+		}
+		series1.getData().add(new XYChart.Data<Number,String>(data[1][data[0].length-2],"> "+data[0][data[0].length-1]));
+
 		bc.getData().addAll(series1);
 		
 		return bc;
